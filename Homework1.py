942d5fc622c6cf7bb018ffa225071df2f0c9a69a
Ugurcan Dede <ugurcan.dede@outlook.com.tr>
2020-12-22 13:20:53 +0300

    Homework 1

---
+++ Homework1.py
@@ -0,0 +1,11 @@
+"""
+        Global AI Hub  
+    Introduction to Python 
+    Homework I — Ugurcan Dede
+"""
+
+i = 1
+while i < 6:
+    v = input("Please input a value:")
+    print('You entered {}, and the type of this value {}'.format(v,type(v)))
+    i+= 1
